48b2822488d74a7cbac9dc99543e019036a5e457
maomao <maomaohqm@gmail.com>
2020-07-31 20:36:20 +0800

    add swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,42 +1,45 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav">
-
       <template v-slot:center>
         <div>购物街</div>
       </template>
-
     </nav-bar>
+
+    <home-swiper :banner="banner"></home-swiper>
+    <recommend-view :recommends="recommend"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
-import {getHomeMultidata} from 'network/home.js'
+import { getHomeMultidata } from "network/home.js";
+import HomeSwiper from "./childComponent/HomeSwiper";
+import RecommendView from './childComponent/RecommendView'
+
 export default {
   name: "Home",
   components: {
     NavBar,
+    HomeSwiper,
+    RecommendView
   },
-  data(){
+  data() {
     return {
-      banner:[],
-      dKeyword:null,
-      keywords:null,
-      recommend:[]
-    }
+      banner: [],
+      dKeyword: null,
+      keywords: null,
+      recommend: [],
+    };
   },
-  created(){
-    getHomeMultidata().then(
-      res=>{
-        this.banner = res.data.banner,
-        this.recommend=res.data.recommend,
-        this.dKeyword = res.data.dKeyword,
-        this.keywords = res.data.keywords
-      }
-    )
-    
-  }
+  created() {
+    getHomeMultidata().then((res) => {
+      (this.banner = res.data.banner.list),
+        (this.recommend = res.data.recommend.list),
+        (this.dKeyword = res.data.dKeyword),
+        (this.keywords = res.data.keywords);
+    });
+  },
 };
 </script>
 
---
+++ src/views/home/childComponent/HomeSwiper.vue
@@ -0,0 +1,32 @@
+<template>
+    <swiper>
+      <swiper-item v-for="item in banner" :key="item.index">
+        <a :href="item.link">
+          <img :src="item.image" :alt="item.title">
+        </a>
+      </swiper-item>
+    </swiper>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from "components/common/swiper";
+
+    export default {
+        name:'HomeSwiper',
+        props:{
+            banner:{
+                type:Array ,
+                default(){
+                    return []
+                }
+            }
+        },
+        components:{
+            Swiper, SwiperItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
